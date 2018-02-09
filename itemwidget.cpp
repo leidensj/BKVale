9f1a0b68fbe5cb3f72e6b04f2ffccbfccd07fa0a
leidensj <jfleidens@gmail.com>
2018-02-09 15:56:29 -0200

    corrigido nomes colunas item table

--- itemwidget.cpp
+++ itemwidget.cpp
@@ -73,18 +73,18 @@ void ItemWidget::setDatabase(QSqlDatabase db)
 {
   ItemTableModel* model = new ItemTableModel(m_database, db);
   QVector<SqlTableColumn> columns;
-  columns.push_back(SqlTableColumn(true, false, "_ID", "Id", QHeaderView::ResizeMode::ResizeToContents));
-  columns.push_back(SqlTableColumn(false, true, "_NAME", "Nome", QHeaderView::ResizeMode::Stretch));
-  columns.push_back(SqlTableColumn(true, false, "_CATEGORYID", "Ícone", QHeaderView::ResizeMode::ResizeToContents));
-  columns.push_back(SqlTableColumn(false, false, "_UNITY", "Ícone", QHeaderView::ResizeMode::ResizeToContents));
-  columns.push_back(SqlTableColumn(true, false, "_PACKAGE_UNITY", "Ícone", QHeaderView::ResizeMode::ResizeToContents));
-  columns.push_back(SqlTableColumn(true, false, "_PACKAGE_AMMOUNT", "Ícone", QHeaderView::ResizeMode::ResizeToContents));
-  columns.push_back(SqlTableColumn(false, false, "_DETAILS", "Ícone", QHeaderView::ResizeMode::ResizeToContents));
-  columns.push_back(SqlTableColumn(true, false, "_CODE", "Ícone", QHeaderView::ResizeMode::ResizeToContents));
-  columns.push_back(SqlTableColumn(true, false, "_ICON", "Ícone", QHeaderView::ResizeMode::ResizeToContents));
-  columns.push_back(SqlTableColumn(true, false, "_AVAILABLE_AT_NOTES", "Ícone", QHeaderView::ResizeMode::ResizeToContents));
-  columns.push_back(SqlTableColumn(true, false, "_AVAILABLE_AT_SHOP", "Ícone", QHeaderView::ResizeMode::ResizeToContents));
-  columns.push_back(SqlTableColumn(true, false, "_AVAILABLE_AT_CONSUMPTION", "Ícone", QHeaderView::ResizeMode::ResizeToContents));
+  columns.push_back(SqlTableColumn(true, false, "_ID", tr("Id"), QHeaderView::ResizeMode::ResizeToContents));
+  columns.push_back(SqlTableColumn(false, true, "_NAME", tr("Nome"), QHeaderView::ResizeMode::Stretch));
+  columns.push_back(SqlTableColumn(true, false, "_CATEGORYID", tr("Categoria"), QHeaderView::ResizeMode::ResizeToContents));
+  columns.push_back(SqlTableColumn(false, false, "_UNITY", tr("Unidade"), QHeaderView::ResizeMode::ResizeToContents));
+  columns.push_back(SqlTableColumn(true, false, "_PACKAGE_UNITY", tr("Unidade da Embalagem"), QHeaderView::ResizeMode::ResizeToContents));
+  columns.push_back(SqlTableColumn(true, false, "_PACKAGE_AMMOUNT", tr("Quantidade da Embalagem"), QHeaderView::ResizeMode::ResizeToContents));
+  columns.push_back(SqlTableColumn(false, false, "_DETAILS", tr("Detalhes"), QHeaderView::ResizeMode::ResizeToContents));
+  columns.push_back(SqlTableColumn(true, false, "_CODE", tr("Código"), QHeaderView::ResizeMode::ResizeToContents));
+  columns.push_back(SqlTableColumn(true, false, "_ICON", tr("Ícone"), QHeaderView::ResizeMode::ResizeToContents));
+  columns.push_back(SqlTableColumn(true, false, "_AVAILABLE_AT_NOTES", tr("Notas"), QHeaderView::ResizeMode::ResizeToContents));
+  columns.push_back(SqlTableColumn(true, false, "_AVAILABLE_AT_SHOP", tr("Compras"), QHeaderView::ResizeMode::ResizeToContents));
+  columns.push_back(SqlTableColumn(true, false, "_AVAILABLE_AT_CONSUMPTION", tr("Consumo"), QHeaderView::ResizeMode::ResizeToContents));
   m_database->set(model, "_ITEMS", columns);
 }
 

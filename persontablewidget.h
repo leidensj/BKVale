ba5da1a0169a713b5d196eaee537f077bea97406
leidensj <jfleidens@gmail.com>
2018-11-10 22:21:45 -0200

    person table

--- persontablewidget.h
+++ persontablewidget.h
@@ -1,11 +1,32 @@
 #ifndef PERSONTABLEWIDGET_H
 #define PERSONTABLEWIDGET_H
 
+#include "jitemtable.h"
+#include "note.h"
 
-class PersonTableWidget
+class PersonTableWidget : public JTable
 {
+  Q_OBJECT
+
 public:
-  PersonTableWidget();
+  explicit PersonTableWidget(QWidget* parent = nullptr);
+  const JItem& getItem(int row) const;
+
+public slots:
+  void addItem(const JItem& o);
+  void addItemAndLoadPrices(Id supplierId);
+  void addItemAndLoadPricesByBarcode(Id supplierId);
+  double computeTotal() const;
+
+private:
+  mutable NoteItem m_ref;
+  double computePrice(int row) const;
+  double computeSubTotal(int row) const;
+
+protected slots:
+  void update(int row, int column);
+  void itemActivate(int row, int column);
+  void itemDelete(int row, int column);
 };
 
 #endif // PERSONTABLEWIDGET_H

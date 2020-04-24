7234fd34b398570db3e23f29ac8d74c96f6bf872
leidensj <jfleidens@gmail.com>
2020-04-23 21:31:28 -0300

    avanços report

--- purchasereport.cpp
+++ purchasereport.cpp
@@ -57,38 +57,96 @@ void PurchaseReport::updateControls()
 
 void PurchaseReport::process()
 {
-  if (m_filter != nullptr)
+  if (m_filter == nullptr)
+    return;
+
+  QSqlDatabase db(QSqlDatabase::database(POSTGRE_CONNECTION_NAME));
+  QSqlQuery query(db);
+  query.prepare("SELECT _NOTES._NUMBER AS NUMBER," //0
+                "_NOTES._DATE AS _DATE," //1
+                "_NOTES._OBSERVATION AS OBSERVATION," //2
+                "_NOTES._DISCCOUNT AS DISCCOUNT," //3
+                "_NOTES._PAYMENT_METHOD AS PAYMENT," //4
+                "SUPPLIERFORM._NAME AS SUPPLIER," //5
+                "STOREFORM._NAME AS STORE," //6
+                "_PRODUCTS._NAME AS PRODUCT," //7
+                "_PRODUCTS._UNITY AS UNITY," //8
+                "_NOTE_ITEMS._AMMOUNT AS AMMOUNT," //9
+                "_NOTE_ITEMS._PRICE AS PRICE," //10
+                "_NOTE_ITEMS._IS_PACK AS ISPACK," //11
+                "_NOTE_ITEMS._PACK_UNITY AS PACKUNITY," //12
+                "_NOTE_ITEMS._PACK_AMMOUNT AS PACKAMMOUNT," //13
+                "_NOTE_ITEMS._PRICE * _NOTE_ITEMS._AMMOUNT AS SUBTOTAL " //14
+                "FROM _NOTE_ITEMS "
+                "LEFT JOIN _NOTES ON _NOTE_ITEMS._NOTEID = _NOTES._ID "
+                "LEFT JOIN _SUPPLIERS ON _NOTES._SUPPLIERID = _SUPPLIERS._ID "
+                "LEFT JOIN _FORMS AS SUPPLIERFORM ON _SUPPLIERS._FORMID = SUPPLIERFORM._ID "
+                "LEFT JOIN _STORES ON _NOTES._STOREID = _STORES._ID "
+                "LEFT JOIN _FORMS AS STOREFORM ON _STORES._FORMID = STOREFORM._ID "
+                "LEFT JOIN _PRODUCTS ON _NOTE_ITEMS._PRODUCTID = _PRODUCTS._ID");
+  if (query.exec() && query.next())
   {
-    QFile data("output.txt");
-    if (data.open(QFile::WriteOnly | QFile::Truncate))
+    auto r = new QtRPT(this);
+    r->loadReport(":/reportsxml/purchase.xml");
+    connect(r, &QtRPT::setValue,
+            [&query](const int recNo, const QString paramName, QVariant &paramValue, const int /*reportPage*/)
     {
-      QTextStream out(&data);
-      qDebug() << "Inicio: " << QTime::currentTime().toString("hh:mm:ss.zzz");
-      QSqlDatabase db(QSqlDatabase::database(POSTGRE_CONNECTION_NAME));
-      QSqlQuery query(db);
-      query.prepare("SELECT _NOTES._NUMBER AS NUMBER, _NOTES._DATE AS _DATE, _NOTES._OBSERVATION AS OBSERVATION, _NOTES._DISCCOUNT AS DISCCOUNT, _NOTES._PAYMENT_METHOD AS PAYMENT,"
-                    "SUPPLIERFORM._NAME AS SUPPLIER, STOREFORM._NAME AS STORE,"
-                    "_PRODUCTS._NAME AS PRODUCT, _PRODUCTS._UNITY AS UNITY,"
-                    "_NOTE_ITEMS._AMMOUNT AS AMMOUNT, _NOTE_ITEMS._PRICE AS PRICE, _NOTE_ITEMS._IS_PACK AS ISPACK, _NOTE_ITEMS._PACK_UNITY AS PACKUNITY, _NOTE_ITEMS._PACK_AMMOUNT AS PACKAMMOUNT,"
-                    "_NOTE_ITEMS._PRICE * _NOTE_ITEMS._AMMOUNT AS SUBTOTAL "
-                    "FROM _NOTE_ITEMS "
-                    "LEFT JOIN _NOTES ON _NOTE_ITEMS._NOTEID = _NOTES._ID "
-                    "LEFT JOIN _SUPPLIERS ON _NOTES._SUPPLIERID = _SUPPLIERS._ID "
-                    "LEFT JOIN _FORMS AS SUPPLIERFORM ON _SUPPLIERS._FORMID = SUPPLIERFORM._ID "
-                    "LEFT JOIN _STORES ON _NOTES._STOREID = _STORES._ID "
-                    "LEFT JOIN _FORMS AS STOREFORM ON _STORES._FORMID = STOREFORM._ID "
-                    "LEFT JOIN _PRODUCTS ON _NOTE_ITEMS._PRODUCTID = _PRODUCTS._ID");
-      if (query.exec())
+      qDebug() << "recNo " <<recNo << " query.at() " << query.at();
+      if (recNo == (query.at() + 1))
+        query.next();
+      else if (recNo == (query.at() - 1))
+        query.previous();
+
+      if (query.at() == recNo)
       {
-        int lastNumber = -1;
-        while (query.next())
+        if (paramName == "number")
         {
-          if (query.value(0).toInt() != lastNumber)
-            out << query.value("SUPPLIER").toString() << Qt::endl;
-          lastNumber = query.value(0).toInt();
+          paramValue = query.value(0);
+        }
+        else if(paramName == "_date")
+        {
+          paramValue = query.value(1).toDate().toString("dd/MM/yyyy dddd");
+        }
+        else if(paramName == "payment")
+        {
+          paramValue = Purchase::st_paymentText((Purchase::PaymentMethod)query.value(4).toInt());
+        }
+        else if(paramName == "supplier")
+        {
+          paramValue = query.value(5);
+        }
+        else if(paramName == "store")
+        {
+          paramValue = query.value(6);
+        }
+        else if(paramName == "product")
+        {
+          paramValue = query.value(7);
+        }
+        else if(paramName == "ammount")
+        {
+          paramValue = Data::strAmmount(query.value(9).toDouble()) +
+                       (query.value(11).toBool()
+                        ? query.value(12).toString()
+                        : query.value(8).toString()) +
+                       " x R$" + Data::strMoney(query.value(10).toDouble());
+        }
+        else if(paramName == "subtotal")
+        {
+          paramValue = query.value(14);
+        }
+        else if(paramName == "disccount")
+        {
+          paramValue = query.value(3);
         }
       }
-      qDebug() << "Fim: " << QTime::currentTime().toString("hh:mm:ss.zzz");
-    }
+    });
+    int count = query.size();
+    connect(r, &QtRPT::setDSInfo,[count](DataSetInfo& ds)
+    {
+      if (ds.reportPage == 0)
+        ds.recordCount = count;
+    });
+    r->printExec();
   }
 }
